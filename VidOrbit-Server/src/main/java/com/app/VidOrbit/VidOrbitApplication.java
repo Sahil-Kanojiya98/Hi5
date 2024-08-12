e27db2948e81ce1ebdef455789effd43196cc22c
sahil <sahilkanojiya1000@gmail.com>
2024-08-13 00:01:36 +0530

    VidOrbit

---
+++ VidOrbit-Server/src/main/java/com/app/VidOrbit/VidOrbitApplication.java
@@ -0,0 +1,14 @@
+package com.app.VidOrbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+VidOrbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidOrbitApplication.class, args);
+	}
+
+}
---
+++ VidOrbit-Server/src/test/java/com/app/VidOrbit/VidOrbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.VidOrbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidOrbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
