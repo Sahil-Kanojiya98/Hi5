c43dcff4eeaee69fa06d4ddbc6f92affd483846e
sahil <sahilkanojiya1000@gmail.com>
2025-01-04 16:48:56 +0530

    rename project

---
+++ Hi5-Server/src/main/java/com/app/Hi5/Hi5Application.java
@@ -0,0 +1,11 @@
+package com.app.Hi5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hi5Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Hi5Application.class, args);
+	}
+}
--- Hi5-Server/src/test/java/com/app/Hi5/VidOrbitApplicationTests.java
+++ Hi5-Server/src/test/java/com/app/Hi5/VidOrbitApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.Echohub;
+package com.app.Hi5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
